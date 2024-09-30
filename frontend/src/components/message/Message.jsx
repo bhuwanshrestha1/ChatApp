9091d24fc7050870be94b9b13c86aa6c0b05ca3b
bhuwanshrestha1 <shresthakritik@gmail.com>
2024-09-30 17:20:01 +0545

    Chatapp_v1.0

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -1,44 +1,28 @@
-import React from 'react';
-import { useAuthContext } from '../../context/AuthContext';
-import useConversation from '../../zustand/useConversation';
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
-    const { authUser } = useAuthContext();
-    const { selectedConversation } = useConversation();
-    const fromMe = message.senderId === authUser._id;
-    const chatClassName = fromMe ? 'chat-end' : 'chat-start';
-    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-    const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-    const formatDateTime = (dateString) => {
-        const date = new Date(dateString);
-        
-        // Format the date
-        const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit' }; 
-        const formattedDate = date.toLocaleDateString([], dateOptions); // Format date
+	const shakeClass = message.shouldShake ? "shake" : "";
 
-        // Format the time
-        const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false }; 
-        const formattedTime = date.toLocaleTimeString([], timeOptions); // Format time
-
-        return `${formattedDate} ${formattedTime}`; // Combine date and time
-    };
-
-    return (
-        <div className={`chat ${chatClassName}`}>
-            <div className='chat-image avatar'>
-                <div className='w-10 rounded-full'>
-                    <img src={profilePic} alt="User avatar" />
-                </div>
-            </div>
-            <div className={`chat-bubble text-white ${bubbleBgColor}`}>
-                {message.message}
-            </div>
-            <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>
-                {formatDateTime(message.createdAt)}
-            </div>
-        </div>
-    );
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
 };
-
 export default Message;
